38f823b386e1fe41d5d723bf0d17ecfe1b4a1d83
MRstiven232004 <stiven20042316@gmail.com>
2025-11-24 06:49:06 -0500

    p1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+    <div>hola</div>
+      <div className='bg-sky-700'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
