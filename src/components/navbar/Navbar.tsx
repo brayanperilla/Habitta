d33ce3fbfd3895ef7771fbcd7b21388e1a6b4a9b
MRstiven232004 <stiven20042316@gmail.com>
2025-12-10 14:40:27 -0500

    v2.3-css
    se cambian los valores de las dimensiones del navbar y del footer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,28 +5,30 @@ import logoSF from '../../assets/images/logoSF.png'
 function Navbar() {
   return (
     <nav className="navbar">
-      {/* Logo */}
-      <div className="navbar__logo">
-        <img src={logoSF} alt="Habitta logo" className="navbar__logo-img" />
-      </div>
+      <div className="navbar__inner">
+        {/* Logo */}
+        <div className="navbar__logo">
+          <img src={logoSF} alt="Habitta logo" className="navbar__logo-img" />
+        </div>
 
-      {/* Navigation Links */}
-      <div className="navbar__links">
-        {navbarLinks.map((link) => (
-          <a key={link.label} href={link.href} className="navbar__link">
-            <span className="navbar__link-text">{link.label}</span>
-          </a>
-        ))}
-      </div>
+        {/* Navigation Links */}
+        <div className="navbar__links">
+          {navbarLinks.map((link) => (
+            <a key={link.label} href={link.href} className="navbar__link">
+              <span className="navbar__link-text">{link.label}</span>
+            </a>
+          ))}
+        </div>
 
-      {/* Publish and User Icons */}
-      <div className="navbar__actions">
-        <button className="navbar__publish-btn">
-          <span>+ Publicar</span>
-        </button>
-        <button className="navbar__user-btn">
-          <span>👤</span>
-        </button>
+        {/* Publish and User Icons */}
+        <div className="navbar__actions">
+          <button className="navbar__publish-btn">
+            <span>+ Publicar</span>
+          </button>
+          <button className="navbar__user-btn">
+            <span>👤</span>
+          </button>
+        </div>
       </div>
     </nav>
   );
