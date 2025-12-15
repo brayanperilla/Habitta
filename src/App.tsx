3806c672a8276dc6c85fda29e3e9ce509e9fad72
MRstiven232004 <stiven20042316@gmail.com>
2025-12-15 11:28:49 -0500

    agregacion de rutas faltantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
 import Properties from "./pages/properties/Properties";
 import RegisterPropeties from "./pages/registerpropeties/RegisterPropeties";
 import "./pages/registerpropeties/styleRegisterP.css";
